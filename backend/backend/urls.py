66e46d7cf31388e55e84a1b9a0f78a18437bd17a
wei xiaobin <483900336@qq.com>
2026-01-29 19:54:47 +0800

    测试前后端打包，删除前端模板

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,10 +14,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', include('web.urls'))
 ]
+
+# 仅限开发阶段使用。生产阶段需要在nginx里配置。
+if settings.DEBUG:
+    urlpatterns += static(
+        '/assets/',
+        document_root=settings.BASE_DIR / 'static/frontend/assets'
+    )
+    urlpatterns += static(
+        '/media/',
+        document_root=settings.MEDIA_ROOT
+    )
