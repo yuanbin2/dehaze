de2265c8339845c53e32da0c671b7f22ddb83ebd
wei xiaobin <483900336@qq.com>
2026-02-02 15:12:32 +0800

    更新数据库

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from web.models.user import UserProfile
+
+
 # Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',)
