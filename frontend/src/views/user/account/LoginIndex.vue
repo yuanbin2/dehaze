710aa8a03fcf2419eac552a6cc8a653062a8940f
wei xiaobin <483900336@qq.com>
2026-02-02 14:32:04 +0800

    登录注册前端界面

--- frontend/src/views/user/account/LoginIndex.vue
+++ frontend/src/views/user/account/LoginIndex.vue
@@ -3,7 +3,22 @@
 </script>
 
 <template>
-登录
+  <div class="flex justify-center mt-30">
+    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+      <legend class="fieldset-legend">Login</legend>
+
+      <label class="label">用户名</label>
+      <input type="email" class="input" placeholder="Username" />
+
+      <label class="label">密码</label>
+      <input type="password" class="input" placeholder="Password" />
+
+      <button class="btn btn-neutral mt-4">Login</button>
+      <RouterLink :to="{name: 'user-account-register-index'}" class="flex justify-end text-base">注册</RouterLink>
+      
+    </fieldset>
+  </div>
+
 </template>
 
 <style scoped>
